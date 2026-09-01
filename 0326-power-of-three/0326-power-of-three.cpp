9334b7d79fb168844e0afe76bf0e97837b8f29bc
Vishal Gupta <vgupta30122@gmail.com>
2026-09-01 22:17:25 +0530

    Time: 0 ms (100%), Space: 9 MB (43.86%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+           if (n <= 0)
+            return false;
+
+        // return (n & (n - 1)) == 0;
+        while(n%3==0){
+            n=n/3;
+            // n==1;
+
+        }
+        return n==1;;
+        
+    }
+};
