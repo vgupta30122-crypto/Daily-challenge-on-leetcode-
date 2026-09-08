c7b0c15453390b0b3781665396d51b28ff74b1a6
Vishal Gupta <vgupta30122@gmail.com>
2026-09-08 17:56:04 +0530

    Time: 641 ms (5.13%), Space: 9.3 MB (5.52%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count =0;;
+        for(int i=1000;i<=n;i++){
+           
+             count =count+ to_string(i).length() / 4;
+             //ye integer i ko string mein convert karta hai.
+        }
+        return count;
+        
+    }
+};
