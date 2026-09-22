3e8e46635fdec6e0ebb565349aa9dd0037a00a89
Vishal Gupta <vgupta30122@gmail.com>
2026-09-22 22:03:11 +0530

    Time: 0 ms (100%), Space: 10.8 MB (89.23%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,23 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     // vector<int> inorderTraversal(TreeNode* root) {
+        
+//     // }
+//     void inorder(TreeNode* root,vector<int> & ans){
+//     if(root ==NULL) return ; // base case 
+   
+//     inorder (root->left,ans);// left 
+//      ans.push_back(root->val) ; // root 
+//      inorder (root->right,ans);// right
+// }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         inorder(root,ans);
+//         return ans;
+
+        
+//     }
+// };
+
+
 class Solution {
 public:
-    // vector<int> inorderTraversal(TreeNode* root) {
-        
-    // }
-    void inorder(TreeNode* root,vector<int> & ans){
-    if(root ==NULL) return ; // base case 
-   
-    inorder (root->left,ans);// left 
-     ans.push_back(root->val) ; // root 
-     inorder (root->right,ans);// right
-}
-    vector<int> inorderTraversal(TreeNode* root) {
+    
+    vector<int> inorderTraversal(TreeNode* root){
         vector<int> ans;
-        inorder(root,ans);
-        return ans;
+        stack<TreeNode*>st;
+        TreeNode* node = root;
+        while(st.size()>0 || node){
+            if(node){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                TreeNode* temp = st.top();
+                st.pop();
+                ans.push_back(temp->val);
+                node = temp ->right;
+            }
+        }
+     return ans;
 
         
     }
 };
+
+
+
+
