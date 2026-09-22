6a263825fcd080de85fc7b4ff8015a1084c7ac65
Vishal Gupta <vgupta30122@gmail.com>
2026-09-22 23:47:55 +0530

    Time: 0 ms (100%), Space: 14.7 MB (99.72%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,34 +9,66 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // .............. method 1 ............. nth level and level order 
+// class Solution {
+// public:
+// int levels(TreeNode* root){
+//     if(root ==NULL) return 0;
+//     return  1 + max(levels(root ->left),levels(root ->right));
+// }
+// void nthLevel(TreeNode* root ,int curr,int level,vector<int> &ans) {
+//     if (root == NULL) return; // base case 
+//     if(curr == level){
+//      ans[curr-1]=root->val; // root 
+//         return ;
+//       }
+      
+//         nthLevel(root->left,curr+1,level,ans); // left
+//          nthLevel(root->right ,curr+1,level,ans);// right  
+
+// }
+//    void levelOrder(TreeNode* root,vector<int> &ans){
+//     int n = ans.size();
+//     for(int i=1;i<=n;i++){
+//         nthLevel(root,1,i,ans);
+//         cout<<"\n";
+//     }
+//    }
+
+//     vector<int> rightSideView(TreeNode* root) {
+//          vector<int> ans(levels(root),0);
+//          levelOrder(root, ans);
+//          return ans;
+//     }
+// };
+
+// .............. method2     .............  using preorder and post in order 
 class Solution {
 public:
 int levels(TreeNode* root){
     if(root ==NULL) return 0;
     return  1 + max(levels(root ->left),levels(root ->right));
 }
-void nthLevel(TreeNode* root ,int curr,int level,vector<int> &ans) {
-    if (root == NULL) return; // base case 
-    if(curr == level){
-     ans[curr-1]=root->val; // root 
-        return ;
-      }
-      
-        nthLevel(root->left,curr+1,level,ans); // left
-         nthLevel(root->right ,curr+1,level,ans);// right  
 
-}
-   void levelOrder(TreeNode* root,vector<int> &ans){
-    int n = ans.size();
-    for(int i=1;i<=n;i++){
-        nthLevel(root,1,i,ans);
-        cout<<"\n";
-    }
+   void preorder(TreeNode* root , vector<int> & ans , int level){
+    if(root == NULL) return ;
+    ans[level] = root ->val;
+    preorder(root->left , ans, level+1);
+    preorder(root->right , ans, level+1);
    }
 
     vector<int> rightSideView(TreeNode* root) {
          vector<int> ans(levels(root),0);
-         levelOrder(root, ans);
+         preorder(root, ans,0);
          return ans;
     }
 };
+
+
+
+
+
+
+
+
+
